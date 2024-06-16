8266a2178c52ce62fec89c300b015339a83ec7a7
BhumikaSaini1999 <sainibhumika1624@gmail.com>
2024-06-16 16:53:57 +0530

    checkout form added

--- frontend/angular-ecommerce/src/app/app.routes.ts
+++ frontend/angular-ecommerce/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 //app.routes.ts: Defines the routes for your application.
@@ -11,6 +12,7 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
 
 //Specifying routes from most specific to generic
 export const routes: Routes = [
+    {path: 'checkout', component: CheckoutComponent},
     {path: 'cart-details', component: CartDetailsComponent},
     {path: 'products/:id', component: ProductDetailsComponent},
     {path: 'search/:keyword', component: ProductListComponent},
