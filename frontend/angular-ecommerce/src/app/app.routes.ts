956e6b8438eb3e2175680583462519c9d9621450
BhumikaSaini1999 <bhumikasaini189@gmail.com>
2024-06-23 00:42:51 +0530

    members component using AuthGuard implemented

--- frontend/angular-ecommerce/src/app/app.routes.ts
+++ frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import OktaAuth from '@okta/okta-auth-js';
+import { Injector } from '@angular/core';
 
 
 //app.routes.ts: Defines the routes for your application.
@@ -12,8 +15,20 @@ import { LoginComponent } from './components/login/login.component';
 //app.component.ts: Uses RouterOutlet to handle the routing.
 //product-list.component.ts: Ensures the component is standalone
 
+function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector){
+//Use injector to access any service available within your application
+const router = injector.get(Router);
+
+//Redirect the user to your custom login page
+router.navigate(['/login']);
+}
+
 //Specifying routes from most specific to generic
 export const routes: Routes = [
+    //if authenticated give access to route else send to login page
+    {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard], data:{
+      onAuthRequired: sendToLoginPage
+    } },
     {path: 'login/callback', component: OktaCallbackComponent },
     {path: 'login', component: LoginComponent},
     {path: 'checkout', component: CheckoutComponent},
---
+++ frontend/angular-ecommerce/src/app/components/members-page/members-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './members-page.component.html',
+  styleUrl: './members-page.component.css'
+})
+export class MembersPageComponent implements OnInit{
+
+  constructor(){}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
+
